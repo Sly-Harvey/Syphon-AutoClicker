25c35baa2d1e23f5c7f3decfb26a54351535de88
Harvey Jacobs-Grant <mheharvey@gmail.com>
2022-11-16 00:52:11 +0000

    updated images

--- Syphon AutoClicker/resource.h
+++ Syphon AutoClicker/resource.h
@@ -1,6 +1,6 @@
 //{{NO_DEPENDENCIES}}
 // Microsoft Visual C++ generated include file.
-// Used by Syphon AutoClicker V2.rc
+// Used by Syphon AutoClicker.rc
 //
 #define IDI_ICON1                       101
 
